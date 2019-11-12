5c491f00bb5384515d3891f302f23660f7f90ccf
Vanna Winland <vgwinland@gmail.com>
2019-11-12 21:35:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -103,4 +103,15 @@ class Student
       self.new_from_db(row)
     end.first
   end
+
+  def self.all_students_in_grade_X
+    sql = <<-SQL 
+      SELECT * FROM students 
+      WHERE GRADE = ?
+      ORDER BY students.id
+
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+    end
+  end
 end
