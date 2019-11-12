d61516774ee34d81bfc61f7a21d15a61516348e6
Vanna Winland <vgwinland@gmail.com>
2019-11-12 21:32:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -92,10 +92,10 @@ class Student
   end
 
   def .first_student_in_grade_10
-    sql = <<-SQL 
-      SELECT * FROM students 
-      WHERE grade = 10 
-      ORDER BY students.id 
-      LIMIT 1 
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT 1
     SQL
 end
